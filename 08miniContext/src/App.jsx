787eaeda32824c01ad9e3c82911703166c90c944
Anas-1201aj <aj1201z.zanasjaved>
2026-08-22 23:32:22 +0530

    context Provider mini project

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import UserContext from './context/UserContext'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+
+function App() {
+
+  return (
+    <UserContextProvider>
+   <h1>context Api</h1>
+   <Login />
+   < Profile />
+   </UserContextProvider>
+  )
+}
+
+export default App
