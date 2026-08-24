bac9ae704409abeeae0a96c106d06e61388e048d
Anas-1201aj <aj1201z.zanasjaved>
2026-08-25 02:43:23 +0530

    Redux Tool Kit with Todo Project

---
+++ 11reduxToolKitTodo/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+
+function App() {
+
+  return (
+   <>
+   <h1>Todo With Redux</h1>
+   <AddTodo/>
+
+   <Todos/>
+   </>
+  )
+}
+
+export default App
